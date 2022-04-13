8d19f9691526058ea57fc9b42fd49ba9aebbb068
alexZaicev <alex.zaicef@gmail.com>
2022-04-13 12:50:35 +0100

    setup for ubuntu_v20.04

---
+++ README.md
@@ -0,0 +1,11 @@
+## Description
+
+This repository contains automated setup scripts for setting up fresh installation of the following OS systems:
+- Ubuntu v20.04
+
+## Components that will be installed
+- Basic system components (e.g. net-tools for Ubuntu)
+- Vim editor with common configuration file `$HOME/.vimrc`
+- Git with default editor set to `vim`
+- Python 3 set as default
+- Docker and Docker compose
---
+++ setup_ubuntu_v20.04.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+echo "Running setup script for Ubuntu v20.04"
+echo "#######################################################"
+echo ""
+
+echo "Step 1: Update system after fresh install..."
+echo ""
+set -eux ; \
+    sudo apt-get update ; \
+    sudo apt-get upgrade -y ;
+
+echo "Step 2: Install basic components..."
+echo ""
+set -eux ; \
+    sudo apt-get update ; \
+    sudo apt-get install -y --no-install-recommends \
+        curl \
+        wget \
+        vim \
+        zip \
+        gnupg \
+        unzip \
+        jq \
+        graphviz \
+        ca-certificates \
+        openssh-client \
+        openssh-server \
+        net-tools \
+        netcat \
+        sed \
+        lsb-release \
+        software-properties-common \ 
+        apt-transport-https \
+        gnupg-agent ;
+
+echo "Step 3: Install Git..."
+echo ""
+set -eux ; \
+    sudo apt-get update ; \
+    sudo apt-get install -y --no-install-recommends git ; \
+    git config --global core.editor "vim" ; 
+
+echo "Step 4: Install Python..."
+echo ""
+set -eux ; \
+    sudo apt-get update ; \
+    sudo apt-get install python3 python3-pip ; \
+    echo "alias python=python3" >> ~/.bash_aliases ; \
+    echo "alias pip=pip3" >> ~/.bash_aliases ; \
+    source ~/.bash_aliases ;
+    
+
+echo "Step 5: Install Docker and Docker Compose..."
+echo ""
+set -eux ; \
+    sudo apt-get update ; \
+    sudo apt-get remove docker docker-engine docker.io containerd runc ; \
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg ; \
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null ; \
+    sudo apt-get update ; \
+    sudo apt-get install docker-ce docker-ce-cli containerd.io ;
+    sudo groupadd docker ; \
+    sudo usermod -aG docker $USER ; \
+    sudo systemctl enable docker ;
+    sudo curl -L "https://github.com/docker/compose/releases/download/2.4.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose ; \
+    sudo chmod +x /usr/local/bin/docker-compose ; \
+
+echo "Step 6: Default configuration files..."
+echo ""
+set -eux ; \
+    curl https://raw.githubusercontent.com/alexZaicev/setup/master/vimrc >> ~/.vimrc ;
+
+echo "Setup finished successfully."
+echo ""
+echo "In order to allow your user to pick up all the changes made by the setup script, we recommend you to re-login."
+echo ""
